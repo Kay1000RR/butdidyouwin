19d5769bf49ffd2260d869c64ac3750aebb7a530
Keinosuke Inoue <inknsk@gmail.com>
2026-02-09 12:16:09 -0800

    Replace site with new file set

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+
 
 export default defineConfig({
   site: 'https://butdidyouwin.com',
@@ -8,27 +8,11 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-      filter: (page) => !page.includes('/404'),
-    }),
+
   ],
   output: 'static',
   compressHTML: true,
   build: {
-    inlineStylesheets: 'auto',
-    assets: '_assets',
-  },
-  vite: {
-    build: {
-      cssMinify: 'lightningcss',
-      rollupOptions: {
-        output: {
-          assetFileNames: '_assets/[name].[hash][extname]',
-        },
-      },
-    },
-  },
+    inlineStylesheets: 'auto'
+  }
 });
